bb72a8414c913fb40e95c83c39b86ce5c58d8809
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2022-03-27 02:37:07 +0500

    adds Appointment Table

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,20 +1,31 @@
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  mode: 'jit',
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    purge: {
+        content: [
+            './pages/**/*.{js,ts,jsx,tsx}',
+            './components/**/*.{js,ts,jsx,tsx}',
+            './layouts/**/*.{js,ts,jsx,tsx}',
+        ],
+        safelist: [
+            'sm:col-start-1',
+            'sm:col-start-2',
+            'sm:col-start-3',
+            'sm:col-start-4',
+            'sm:col-start-5',
+            'sm:col-start-6',
+            'sm:col-start-7'
+        ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    mode: 'jit',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
 
-  ],
+    ],
 }
