de40583a577e1d67a639486e3f03c66656088c93
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2022-03-27 04:47:52 +0500

    adds procedural calendar date n days

--- ui/components/AppointmentTable.tsx
+++ ui/components/AppointmentTable.tsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import {useEffect, useRef} from 'react'
-import {format} from 'date-fns'
+import {addDays, format, getDay} from 'date-fns'
 
 export default function AppointmentTable({appointments, startingHour = 8}: React.PropsWithChildren<any>) {
   const container: any = useRef(null)
@@ -58,42 +58,18 @@ export default function AppointmentTable({appointments, startingHour = 8}: React
             <div
               className="-mr-px hidden grid-cols-7 divide-x divide-gray-100 border-r border-gray-100 text-sm leading-6 text-gray-500 sm:grid">
               <div className="col-end-1 w-14"/>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Mon <span className="items-center justify-center font-semibold text-gray-900">10</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Tue <span className="items-center justify-center font-semibold text-gray-900">11</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span className="flex items-baseline">
-                  Wed{' '}
-                  <span className="items-center justify-center font-semibold text-gray-900">12</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Thu <span className="items-center justify-center font-semibold text-gray-900">13</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Fri <span className="items-center justify-center font-semibold text-gray-900">14</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Sat <span className="items-center justify-center font-semibold text-gray-900">15</span>
-                </span>
-              </div>
-              <div className="flex items-center justify-center py-3">
-                <span>
-                  Sun <span className="items-center justify-center font-semibold text-gray-900">16</span>
-                </span>
-              </div>
+              {
+                Array(7).fill(0).map((_, i) => {
+                  return (
+                    <div className="flex items-center justify-center py-3">
+                      <span>
+                        {format(addDays(new Date(), i), "EEE")} <span
+                          className="items-center justify-center font-semibold text-gray-900">{format(addDays(new Date(), i), "d")}</span>
+                      </span>
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
           <div className="flex flex-auto">
