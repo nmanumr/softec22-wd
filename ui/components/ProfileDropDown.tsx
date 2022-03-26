689dcfda0b25b4dfb96c97cf7f6d991e0278d769
nmanumr <nmanumr@gmail.com>
2022-03-27 00:08:54 +0500

    fixes

--- ui/components/ProfileDropDown.tsx
+++ ui/components/ProfileDropDown.tsx
@@ -44,7 +44,7 @@ export default function ProfileDropDown() {
           </div>
           <Menu.Item>
             {({ active }) => (
-              <LinkTo href="/profile" className={c(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+              <LinkTo href="/accounts/profile" className={c(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                 My Account
               </LinkTo>
             )}
