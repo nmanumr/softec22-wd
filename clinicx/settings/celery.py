e327de964dbdd48eb37f33247f598bad921c28de
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2022-03-26 21:51:13 +0500

    removes rabbitmq

--- clinicx/settings/celery.py
+++ clinicx/settings/celery.py
@@ -1,18 +1,13 @@
 import os
 
-from clinicx.utils.parser import parse_int, parse_bool
-
-RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-RABBITMQ_PORT = parse_int(os.getenv('RABBITMQ_PORT'), 5672)
-RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'clinicx')
-RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+from clinicx.utils.parser import parse_bool
 
 CELERY_TASK_ALWAYS_EAGER = parse_bool(os.getenv('CELERY_TASK_ALWAYS_EAGER'), False)
-CELERY_TASK_SERIALIZER = 'pickle'
-CELERY_RESULT_SERIALIZER = 'pickle'
-CELERY_ACCEPT_CONTENT = ['pickle']
-CELERY_BROKER_URL = f'pyamqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}//'
+# CELERY_TASK_SERIALIZER = 'pickle'
+# CELERY_RESULT_SERIALIZER = 'pickle'
+# CELERY_ACCEPT_CONTENT = ['pickle']
 
 
 def configure_celery(settings):
+    settings['CELERY_BROKER_URL'] = f'redis://{settings["REDIS_HOST"]}:{settings["REDIS_PORT"]}/0'
     settings['CELERY_RESULT_BACKEND'] = f'redis://{settings["REDIS_HOST"]}:{settings["REDIS_PORT"]}?db=0'
