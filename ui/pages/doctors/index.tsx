b773dca63411f0e614da8b03ea1170d214325676
faseehahmad00 <faseehahmad00@gmail.com>
2022-03-26 21:12:01 +0500

    search bar integ

--- ui/pages/doctors/index.tsx
+++ ui/pages/doctors/index.tsx
@@ -1,5 +1,6 @@
 import DashboardLayout
     from "../../layouts/DashboardLayout";
+import SearchBar from "../../components/SearchBar";
 
 const people = [
     {
@@ -32,37 +33,26 @@ const people = [
 export default function Doctors() {
     return (
         <DashboardLayout>
+            <SearchBar/>
             <div className="bg-slate">
                 <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-                    <div className="space-y-12">
                         <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
                         </div>
-                        <ul
-                            role="list"
-                            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
-                        >
+                        <ul role="list" className="sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
                             {people.map((person) => (
-                                <li key={person.name}>
-                                    <div className="space-y-4">
-                                        <div className="aspect-w-3 aspect-h-2">
-                                            <img className="object-cover shadow-lg rounded-lg" src={person.imageUrl}
-                                                 alt=""/>
-                                        </div>
-
-                                        <div className="space-y-2">
-                                            <div className="text-lg leading-6 font-medium space-y-1">
-                                                <div className=" mx-3 flex justify-between">
-                                                    <h3>{person.name}</h3>
-                                                    <p className="text-xs text-gray-700">{Math.floor(Math.random() * 10)}.{Math.floor(Math.random() * 10)} km</p>
-                                                </div>
-                                                <p className="ml-3 text-indigo-600">{person.role}</p>
+                                <li key={person.name} className="py-10 px-6 text-center rounded-lg xl:px-10 xl:text-left">
+                                    <div className="flex">
+                                        <img className="h-20 w-20 rounded-full xl:w-22 xl:h-22 object-cover" src={person.imageUrl} alt="" />
+                                        <div className="space-y-2 ml-8 flex ">
+                                            <div className="font-medium text-lg leading-6 flex flex-col justify-items-start align-start content-start">
+                                                <h3 className="text-black">{person.name}</h3>
+                                                <p className="text-indigo-400">{person.role}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </li>
                             ))}
                         </ul>
-                    </div>
                 </div>
             </div>
         </DashboardLayout>
