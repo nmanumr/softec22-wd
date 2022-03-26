5f284caad3308f42744590907a7b4a5804e3f9a7
faseehahmad00 <faseehahmad00@gmail.com>
2022-03-26 14:56:29 +0500

    fixed login page

--- ui/pages/accounts/login.tsx
+++ ui/pages/accounts/login.tsx
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import {useState} from "react";
 import {useRouter} from "next/router";
+import {Controller} from "react-hook-form";
 
 import LinkTo from "../../components/LinkTo";
 import Button from "../../components/Button";
@@ -20,6 +21,10 @@ export default function Login() {
     }
 
     const onSubmit = async (value: Record<string, any>) => {
+        value.phoneNumber = value.CC + value.phoneNumber;
+        delete value.CC
+        console.log(value)
+        return
         setApiError('');
         setLoading(true);
 
@@ -38,24 +43,39 @@ export default function Login() {
 
     return (
         <CompactLayout title="Sign in to your account">
-            <Form onSubmit={onSubmit} className="space-y-4">
+            <Form onSubmit={onSubmit} className="space-y-4" model={{CC: '+92'}}>
 
                 {apiError && <ErrorMessage error={apiError}/>}
 
-                <FormField pattern={{value: /\+\d{12}/, message: 'Invalid Phone number'}} type="tel" name="phone" label={"Phone"}
+                <FormField pattern={{value: /^\d{10}$/, message: 'Invalid Phone number'}} type="tel" name="phoneNumber"
+                           label={"Phone"}
                            required>
                     {({errors, label, ...props}: FormInputFuncProps) => (
                         <div>
-                            <label htmlFor="name" className="block text-sm font-medium text-gray-700">{label}</label>
-                            <input
-                                id="email" placeholder="+92 000 0000000" autoComplete="tel" {...props}
-                                className="appearance-none shadow-sm block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 sm:text-sm focus:z-10 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500"
-                            />
+                            <label htmlFor="name"
+                                   className="block text-sm font-medium text-gray-700">{label}</label>
+                            <div className="flex space-x-4">
+                                <Controller
+                                    name="CC"
+                                    render={({field: {value, onChange}}) => (
+                                        <select value={value} onChange={onChange}
+                                                className="appearance-none shadow-sm block  pl-3  pr-8 py-2 border border-gray-300 rounded-md placeholder-gray-400 sm:text-sm focus:z-10 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500">
+                                            <option>+92</option>
+                                            <option>+922</option>
+                                        </select>
+                                    )}
+                                />
+
+                                <input
+                                    id="email" placeholder="300 0000000" autoComplete="tel" {...props}
+                                    className="appearance-none shadow-sm block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 sm:text-sm focus:z-10 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500"
+                                />
+                            </div>
+
                             {errors && <p className="text-xs mt-1.5 text-red-600">{errors.message}</p>}
                         </div>
                     )}
                 </FormField>
-
                 <FormField name="password" type="password" label={'Password'} required>
                     {({errors, label, ...props}: FormInputFuncProps) => (
                         <div>
@@ -69,7 +89,6 @@ export default function Login() {
                         </div>
                     )}
                 </FormField>
-
                 <div className="flex items-center">
                     <div className="text-sm">
                         Don&apos;t have account?&nbsp;
