a5f27a8142de972c5cdc121e07888679a1152476
faseehahmad00 <faseehahmad00@gmail.com>
2022-03-27 06:46:16 +0500

    patient history fix

--- ui/pages/patient/history.tsx
+++ ui/pages/patient/history.tsx
@@ -1,14 +1,17 @@
-import DashboardLayout from "../../layouts/DashboardLayout";
-import Button from "../../components/Button";
-import PageHeader from "../../components/PageHeader";
 import React from "react";
 import {router} from "next/client";
+
+import Button from "../../components/Button";
+import PageHeader from "../../components/PageHeader";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+
 
 export default function History() {
   return (
     <DashboardLayout>
       <PageHeader title="Patient History">
-        <Button onClick={()=> router.push('/patient/add-history')} kind="secondary">Add History</Button>
+        <Button href="/patient/add-history" kind="secondary">Add History</Button>
       </PageHeader>
     </DashboardLayout>
   )
