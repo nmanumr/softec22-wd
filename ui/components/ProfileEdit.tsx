c798edf37699e54a1d71a2841024efa0ee16d177
faseehahmad00 <faseehahmad00@gmail.com>
2022-03-27 10:06:18 +0500

    editform fx

--- ui/components/ProfileEdit.tsx
+++ ui/components/ProfileEdit.tsx
@@ -143,6 +143,22 @@ export default function ProfileEdit() {
         </FormField>
       )}
 
+      {data.type === 'doctor' && (
+        <FormField type="text" name="clinicName" label={"Clinic Name"} required>
+          {({ errors, label, ...props }: FormInputFuncProps) => (
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-700">{label}</label>
+              <input
+                {...props}
+                className="appearance-none shadow-sm block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 sm:text-sm focus:z-10 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500"
+              />
+              {errors && <p className="text-xs mt-1.5 text-red-600">{errors.message}</p>}
+            </div>
+          )}
+        </FormField>
+      )}
+
       <div className="pt-2">
         <Button loading={loading} className="w-full" type="submit">Save</Button>
       </div>
